114424996c9caee9c65080fb87532c4bf01f7142
Carlos <canutdominguez@gmail.com>
2021-05-21 22:43:17 +0200

    primera ejecución o actualización (hdfs)
    diferenciar entre primera ejecución de la extracción de datos o actualización

--- save_in_hdfs.sh
+++ save_in_hdfs.sh
@@ -1,7 +1,15 @@
 start-dfs.sh
 git pull
 
-local_stonks=$(git diff --name-only HEAD^1..HEAD | awk -F" " '{split($1,a,"."); if(a[2] == "csv") print($1)}')
+read -p "¿Es esta tu primera vez o simplemente estas actualizando los datos? (yes: primera vez, no: actualizar datos)" primera_ejecucion
+
+if $primera_ejecucion = 'yes'
+then
+	local_stonks=$(ls stonks/)
+else
+	local_stonks=$(git diff --name-only HEAD^1..HEAD | awk -F" " '{split($1,a,"."); if(a[2] == "csv") print($1)}')
+fi
+
 
 for stonk in $local_stonks; do
 	if hdfs dfs -test -e /stonks/$stonk
