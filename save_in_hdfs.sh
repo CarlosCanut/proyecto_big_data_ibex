4b1513e848bd609f9679d7eef9f6a5b6e3b6fcc6
Carlos <canutdominguez@gmail.com>
2021-05-21 22:52:32 +0200

    primera ejecucion

--- save_in_hdfs.sh
+++ save_in_hdfs.sh
@@ -1,9 +1,9 @@
 start-dfs.sh
 git pull
 
-read -p "¿Es esta tu primera vez o simplemente estas actualizando los datos? (1: primera vez, 2: actualizar datos)" primera_ejecucion
+read -p "¿Es esta tu primera vez o simplemente estas actualizando los datos? (yes : primera vez, no : actualizar datos)" primera_ejecucion
 
-if $primera_ejecucion = '1'
+if $primera_ejecucion == 'yes'
 then
 	local_stonks=$(ls stonks/)
 else
