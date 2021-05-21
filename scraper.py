f98b32d019d201a9551c183959a753e567da1a19
Carlos <canutdominguez@gmail.com>
2021-05-21 22:15:41 +0200

    2021-05-21

--- scraper.py
+++ scraper.py
@@ -74,8 +74,9 @@ def stonks():
                 writer.writerows(reduced_values)
 
             # # add files to git repo
-            # os.system("git add" + " stonks/all_" + date + ".csv")
-            # os.system("git add" + " stonks/reduced_" + date + ".csv")
+            os.system("ls stonks/all_" + date + ".csv")
+            os.system("git add" + " stonks/all_" + date + ".csv")
+            os.system("git add" + " stonks/reduced_" + date + ".csv")
 
             driver.quit()
         except:
