8008de0bba9b4fa001f140758c844374987d32c4
Carlos <canutdominguez@gmail.com>
2021-05-21 22:34:59 +0200

    data extraction for hdfs

--- save_in_hdfs.sh
+++ save_in_hdfs.sh
@@ -1,6 +1,7 @@
 start-dfs.sh
+git pull
 
-local_stonks=$(ls stonks/)
+local_stonks=$(git diff --name-only HEAD^1..HEAD | awk -F" " '{split($1,a,"."); if(a[2] == "csv") print($1)}')
 
 for stonk in $local_stonks; do
 	if hdfs dfs -test -e /stonks/$stonk
