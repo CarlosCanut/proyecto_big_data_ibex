a82015588c33f5bcdb90d91f0d7432a368be3895
Carlos <canutdominguez@gmail.com>
2021-06-05 19:41:05 +0200

    proyecto reorganizado

---
+++ old_scripts/start.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+clear
+
+dialog --begin 2 2 --inputmenu "hola" 20 20 20 "option1" "option2" 
+
+start-dfs.sh
+git pull
+
+clear 
+
+read -p "¿Quieres cargar todos los datos a hdfs o solo actualizar nuevos datos? (total : todos los datos (.csv), new : actualizar nuevos datos (.csv))" primera_ejecucion
+
+if [ $primera_ejecucion == 'total' ]
+then
+	local_stonks=$(ls -p stonks | grep -v /)
+else
+	local_stonks=$(git diff --name-only HEAD^1..HEAD | awk -F" " '{split($1,a,"."); if(a[2] == "csv") print($1)}')
+fi
+
+
+for stonk in $local_stonks; do
+	echo $stonk
+	if [ $primera_ejecucion == 'total' ]
+	then
+		if hdfs dfs -test -e /stonks/$stonk
+		then
+			echo already exists
+		else
+			hdfs dfs -put stonks/$stonk /stonks/$stonk
+			echo file added
+		fi
+	else
+		if hdfs dfs -test -e /$stonk
+		then
+			echo already exists
+		else
+			hdfs dfs -put $stonk /$stonk
+			echo file added
+		fi
+	fi
+done
+
--- scraper.py
+++ scraper.py
@@ -123,16 +123,16 @@ while True:
             break
 
 # creates daily file
-for day_iter in ['04_21', '05_18', '05_19', '05_20', '05_21', '05_25', '05_26', '05_27', '05_28', '05_31', '06_01']:
+for day_iter in ['04_21', '05_18', '05_19', '05_20', '05_21', '05_25', '05_26', '05_27', '05_28', '05_31', '06_01', '06_02', '06_03', '06_04']:
     day_df = pd.DataFrame(columns=['accion', 'ultima_cotizacion', 'max_sesion', 'min_sesion', 'ultima_actualizacion', 'fecha', 'hora'])
     for hour in ["09_30", "10_30", "11_30", "12_30", "13_30", "14_30", "15_30", "16_30", "17_30", "18_30"]:
-        hour_df = pd.read_csv("stonks/temp/reduced_"+ year + "_" + month + "_" + day_iter + "_" + hour + ".csv")
+        hour_df = pd.read_csv("stonks/temp/reduced_"+ year + "_" + f"{month:02}" + "_" + f"{day_iter:02}"[-2:] + "_" + hour + ".csv")
         hour_df.columns = ['accion', 'ultima_cotizacion', 'max_sesion', 'min_sesion', 'ultima_actualizacion']
-        hour_df['fecha'] = str(year+"/"+ month + "/" + day_iter )
+        hour_df['fecha'] = str(year+"/"+ f"{month:02}" + "/" + f"{day_iter:02}"[-2:] )
         hour_df['hora'] = str(hour)
         #print(hour_df)
         day_df = day_df.append(hour_df, ignore_index=True)
-    day_df.to_csv("stonks/" + year + "_" + month + "_" + day + ".csv", index=False, header=False)
+    day_df.to_csv("stonks/" + year + "_" + f"{day_iter:02}" + ".csv", index=False, header=False)
 
 os.system('git add -A && git commit -m "' + str(datetime.datetime.today())[:10] + '"')
 os.system("git push")
